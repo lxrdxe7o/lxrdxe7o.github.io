7d3d57d3a4b9ba0d6a37d7b437416e23d381beda
xero <ishrak7106@gmail.com>
2026-01-16 03:14:31 +0600

    Updates

--- src/components/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { Hero } from './Hero';
-export { About } from './About';
-export { Skills } from './Skills';
-export { Projects } from './Projects';
-export { Contact } from './Contact';
--- src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from './Button';
-export { Badge } from './Badge';
-export { Card } from './Card';
-export { VectorBackground } from './VectorBackground';
